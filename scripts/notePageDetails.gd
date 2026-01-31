705d99141c166ae53a6d4e4a8bd82e2e462942e9
David Vyhnalik <dvy@packaging-cockpit.com>
2026-01-31 20:10:48 +0100

    added Notebook

---
+++ scripts/notePageDetails.gd
@@ -0,0 +1,10 @@
+class_name NotePageDetails
+
+var icon = "";
+var title = "";
+var description = "";
+
+func _init(details_icon, details_title, details_description) -> void:
+	icon = details_icon;
+	title = details_title;
+	description = details_description;
