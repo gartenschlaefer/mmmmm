c8a5f8dec02a2efd7a909ed3e9a3f38868b0e002
unknown <ole.siemers@tuwien.ac.at>
2026-01-31 18:41:36 +0100

    merge

--- scripts/detective_character.gd
+++ scripts/detective_character.gd
@@ -9,8 +9,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
+	var horizontal_direction := Input.get_axis("left", "right")
+	var vertical_direction := Input.get_axis("up", "down")
 	var direction = Vector2(horizontal_direction, vertical_direction)
 	if direction:
 		velocity = direction * SPEED
