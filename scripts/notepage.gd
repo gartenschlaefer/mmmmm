d920c4a3d983c501e67d6df884bf577c49da6676
David Vyhnalik <dvy@packaging-cockpit.com>
2026-02-01 14:51:46 +0100

    added correct dialogs to npcs

--- scripts/notepage.gd
+++ scripts/notepage.gd
@@ -15,6 +15,7 @@ var charactersFound : Array[Character_Enum.Characters] = [];
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	characterFound(Character_Enum.Characters.PENGUIN);
+	characterFound(Character_Enum.Characters.MOUSE);
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
